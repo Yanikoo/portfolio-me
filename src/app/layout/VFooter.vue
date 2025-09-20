1367b50662d7a531121931defc8d810352c0e0c1
Yanikoo <167081007+Yanikoo@users.noreply.github.com>
2025-09-21 00:22:24 +0300

    НАкидал подвал

---
+++ src/app/layout/VFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer">
+    <div class="container">© {{ copyrightDate }} Yanikoo. All Rights Reserved.</div>
+    <a
+      href="https://github.com/Yanikoo/portfolio-me"
+      target="_blank"
+      >GitHub</a
+    >
+  </footer>
+</template>
+
+<script setup lang="ts">
+import type { Ref } from 'vue'
+
+import { computed } from 'vue'
+
+const CREATION_YEAR = 2025
+
+const copyrightDate: Ref<string> = computed(() => {
+  const currentYear = new Date().getFullYear()
+
+  return currentYear > CREATION_YEAR ? `${CREATION_YEAR} - ${currentYear}` : `${CREATION_YEAR}`
+})
+</script>
+
+<style scoped lang="scss">
+.footer {
+  text-align: center;
+  font-size: 1.4rem;
+  line-height: 2rem;
+  padding: 1.6rem 0;
+}
+</style>
