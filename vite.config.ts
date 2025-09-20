9fe18773747f9c17df33f2e7a9c59e08f7c35018
Yanikoo <167081007+Yanikoo@users.noreply.github.com>
2025-09-21 00:15:12 +0300

    chore: init portfolio project

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/app/styles/mixins.scss" as *;
+        `
+      }
+    }
+  },
+  server: {
+    port: 8080,
+    host: '0.0.0.0'
+  }
+})
